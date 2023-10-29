1c2178d5c07b04a6184e31eb814ee7180b4af1b3
LiamYoun <95008167+liam4806@users.noreply.github.com>
2023-10-29 10:19:04 -0400

    express.js upload

--- frontend/app.js
+++ frontend/app.js
@@ -1,76 +1,66 @@
-
 const express = require("express");
 const path = require("path");
 const methodOverride = require("method-override");
 const keys = require("./configuration/keys");
-const Course = require("./models/course");
-const Student = require("./models/student");
 const bodyParser = require("body-parser");
 const app = express();
 const AWS = require("aws-sdk");
 const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
-const fileUpload = require('express-fileupload');
-const fetch = require("node-fetch");
-app.use(fileUpload());
-// require("dotenv").config();
-app.put('/upload', async ({files},res)=>{
-  AWS.config.update({
-    accessKeyId: keys.amazon.AWS_ACCESS_KEY_ID,
-    secretAccessKey: keys.amazon.AWS_SECRET_ACCESS_KEY,
-    region: "us-east-1"
-  });
-  
-  const s3 = new AWS.S3();
 
-  const fileContent = Buffer.from(files.dt.data);
-  console.log(fileContent)
-  const params = {
-    Bucket: "visitor-student-image-storage",
-    Key: files.dt.name,
-    Body: fileContent,
-  };
-  s3.upload(params, (err,data) =>{
-    if(err){
-      throw err;
-    }
-    res.send({
-      "response_code": 200,
-      "response_message":"Success",
-      "response_data":data,
-    });
-  })
-})
-
-app.use(express.static("public"));
-
-const url = `https://tep4cykdch.execute-api.us-east-1.amazonaws.com/dev/student-image-storage/testp.jpeg`;
-
-
-// const dynamoclinet = new AWS.DynamoDB.DocumentClient();
-// const Table = "student-record";
-
-// const getCharacters = async () => {
-//   const params = {
-//     TableName: Table,
-//   };
-//   const chara = await dynamoclinet.scan(params).promise();
-//   // console.log(chara);
-//   return chara;
-// };
-
-// getCharacters();
-
-
-
+const uuid = require("uuid");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  res.render("home");
+AWS.config.update({
+  accessKeyId: keys.amazon.AWS_ACCESS_KEY_ID,
+  secretAccessKey: keys.amazon.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-1",
+});
+
+const s3 = new AWS.S3();
+let upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "visitor-student-image-storage",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+
+const dynamoclinet = new AWS.DynamoDB.DocumentClient();
+const Table = "student-record";
+
+const getDB = async () => {
+  const params = {
+    TableName: Table,
+  };
+  const dbdata = await dynamoclinet.scan(params).promise();
+  return dbdata;
+};
+
+// getCharacters();
+
+app.put("/upload", upload.single("dt"), async (req, res) => {
+  await res.redirect("/");
+});
+app.get("/", async (req, res) => {
+  let student = await getDB();
+  item = student.Items;
+  for (let i = 0; i < item.length; i++) {
+    console.log(item[i]);
+    console.log("-----");
+  }
+  res.render("home", {
+    studentdata: item,
+  });
 });
 app.get("/person", (req, res) => {
   res.render("persondetail");
--- frontend/models/student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const studentSchema = new Schema({
-    student_name: String,
-    class: String,
-});
-
-module.exports = mongoose.model("Student", studentSchema);
